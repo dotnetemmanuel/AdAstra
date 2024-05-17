b6de0f68d94ebe087880de984ca642a19ac4c236
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-05-17 15:25:39 +0200

    Added Posts page and route using subcategory Id and logic to retrieve posts from said category.

---
+++ AdAstra/Pages/Posts.cshtml.cs
@@ -0,0 +1,30 @@
+using AdAstra.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdAstra.Pages
+{
+    public class PostsModel : PageModel
+    {
+       
+        private readonly AdAstraContext _context;
+
+        public PostsModel(AdAstraContext context)
+        {
+            _context = context;
+        }
+
+        public List<Models.Post> Posts { get; set; }
+        public Models.Category Category { get; set; }
+
+        public async Task OnGetAsync(int subcategoryId)
+        {
+            if(subcategoryId != null)
+            {
+                Category = _context.Categories.Where(c => c.Id == subcategoryId).FirstOrDefault();
+                Posts = await _context.Posts.Where(p => p.CategoryId == subcategoryId).Include(p => p.Category).Include(p=> p.Creator).ToListAsync();
+            }            
+        }
+    }
+}
