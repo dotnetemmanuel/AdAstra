01c9e6cbb272ceaabd77f35fa667f103bf5e550f
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-05-27 14:34:17 +0200

    implemented prophanity filter, post and reply styling, including respomsive layout

--- AdAstra/Helpers.cs
+++ AdAstra/Helpers.cs
@@ -1,4 +1,5 @@
 ﻿using AdAstra.Models;
+using System.Text.RegularExpressions;
 
 namespace AdAstra
 {
@@ -7,7 +8,7 @@ namespace AdAstra
         public static int GetReplyDepth(Models.Reply reply)
         {
             int depth = 0;
-            while(reply.ParentReplyId is not null)
+            while (reply.ParentReplyId is not null)
             {
                 depth++;
                 reply = reply.ParentReply;
@@ -15,5 +16,67 @@ namespace AdAstra
             return depth;
         }
 
+        public static readonly List<string> Prophanities = new()
+        {
+            "ass",
+            "asshole",
+            "bastard",
+            "bitch",
+            "bollocks",
+            "bugger",
+            "bullshit",
+            "crap",
+            "cunt",
+            "damn",
+            "dick",
+            "dickhead",
+            "fag",
+            "faggot",
+            "fuck",
+            "fucker",
+            "fucking",
+            "goddamn",
+            "jerk",
+            "kike",
+            "motherfucker",
+            "nigger",
+            "piss",
+            "prick",
+            "pussy",
+            "shit",
+            "shithead",
+            "shitty",
+            "slut",
+            "spick",
+            "twat",
+            "wanker",
+            "whore",
+            "arse",
+            "bint",
+            "bollocks",
+            "clunge",
+            "cock",
+            "coon",
+            "douche",
+            "knob",
+            "minge",
+            "minger",
+            "nonce",
+            "pillock",
+            "plonker",
+            "tosser"
+        };
+
+        public static string ReplaceProphanities(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            string pattern = string.Join("|", Prophanities.Select(Regex.Escape));
+
+            return Regex.Replace(input, pattern, match => new string('*', match.Length), RegexOptions.IgnoreCase);
+        }
     }
 }
